afdecbc7c8961ef4c93b9dd96647e77b006cfeb8
Chiara Turbati <chiara0494@gmail.com>
2023-08-01 21:35:31 +0200

    Add cors and fix route

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
+
 const app = express();
-const port = 3001;
+const port = 3000;
+
+const bookRouter = require("./routes/books");
 
 /* `require("dotenv").config()` is used to load environment variables from a `.env` file into the
 Node.js process. */
 require("dotenv").config();
+
+// Middleware per consentire le richieste da qualsiasi origine
+app.use(cors());
 
 const mongoURL = `${process.env.DATABASE_URL}`;
 mongoose
@@ -19,11 +26,9 @@ mongoose
 
 app.use(express.json()); // Middleware per consentire a Express di parsare il corpo delle richieste in formato JSON
 
-// Rotte e controller per le operazioni CRUD sui book
-const booksRouter = require("./routes/books");
-app.use("/books", booksRouter);
+app.use("/", bookRouter);
 
 // Avvia il server
-app.listen(port, () => {
-  console.log(`Il server è in esecuzione sulla porta ${port}`);
+app.listen(port, "127.0.0.1", () => {
+  console.log(`The server is running on http://127.0.0.1:${port}`);
 });
