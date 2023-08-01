03dbbe39c28132db266d842c551da8c6f652924b
Chiara Turbati <chiara0494@gmail.com>
2023-08-01 16:24:51 +0200

    Books routes

---
+++ routes/books.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const Book = require("../models/book");
+
+// Getting all
+router.get("/", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Getting One
+router.get("/:id", getBook, (req, res) => {
+  res.json(res.book);
+});
+
+// Creating one
+router.post("/", async (req, res) => {
+  const book = new Book(req.body);
+  try {
+    const newBook = await book.save();
+    res.status(201).json(newBook);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Updating One
+router.put("/:id", async (req, res) => {
+  try {
+    const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(book);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Deleting One
+router.delete("/:id", getBook, async (req, res) => {
+  try {
+    await Book.findByIdAndDelete(req.params.id);
+    res.json({ message: "Deleted Book" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getBook(req, res, next) {
+  let book;
+  try {
+    book = await Book.findById(req.params.id);
+    if (book == null) {
+      return res.status(404).json({ message: "Cannot find book" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.book = book;
+  next();
+}
+
+module.exports = router;
